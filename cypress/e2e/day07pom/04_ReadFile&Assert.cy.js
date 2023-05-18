a4b1cf92a6c02c2175bd981bffcfcc9a0662a374
cevik <suleymancevik64@gmail.com>
2023-05-18 17:02:48 +0300

    cypress

--- cypress/e2e/day07pom/04_ReadFile&Assert.cy.js
+++ cypress/e2e/day07pom/04_ReadFile&Assert.cy.js
@@ -1,14 +1,12 @@
 describe("ReadFile&Assert", () => {
-
     it("Assert", () => {
-
         cy.readFile("./cypress/fixtures/users.json").then((userData) => {
-            
-            // fixtures klasorunun altindaki users.json dosyasini oku ve bir fonk olustur
 
-            expect(userData[3].name).to.equal("Patricia Lebsack"); // userData fonk users.json dosyasindaki indexi 3 olan name i alip equal içine yazilan ile karsilastirip eşit oldugunu dogruluyor
+
+            // *** index yapisini kullanarak 3.index'te bulunan 4.user'a assertion
+            expect(userData[3].name).to.equal("Patricia Lebsack");
             expect(userData[3].username).to.equal("Karianne");
-            expect(userData[3].address.street).to.equal("Hoeger Mall");  // bunlar JSon formatinda oldugu icin pojo class gibi altina indim  address.street
+            expect(userData[3].address.street).to.equal("Hoeger Mall"); // ** inner json'a giderken address.street
             expect(userData[3].company.name).to.equal("Robel-Corkery");
         });
     });
